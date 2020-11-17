1670d02c33d7ebfec8ad00031fc6a441e41af831
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-17 12:54:31 +0530

    Start: Indian State Census Analyzer Problem started

---
+++ src/main/java/IndianStateCensusAnalyzerProblem/IndianStateCensusAnalyzerProblem/App.java
@@ -0,0 +1,13 @@
+package IndianStateCensusAnalyzerProblem.IndianStateCensusAnalyzerProblem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

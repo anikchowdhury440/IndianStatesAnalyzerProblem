4a5130c6f82d1995bc3222191fa6caadee8c4736
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-18 18:23:32 +0530

    Refactor: create a separate jar and include the jar in the census analyzer project

--- src/main/java/StateCensusAnalyzer.java
+++ src/main/java/StateCensusAnalyzer.java
@@ -4,6 +4,10 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Paths;
 import java.util.Iterator;
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
 import CSVBuilderJar.CSVBuilderJar.*;
 public class StateCensusAnalyzer {
 	

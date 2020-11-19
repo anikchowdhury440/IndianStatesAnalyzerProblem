74d42a88923777048f662207806f1b17d3228a3f
anikchowdhury440 <anikchowdhury440@gmail.com>
2020-11-19 10:47:00 +0530

    Refactor: Removed Unused Import

--- src/main/java/StateCensusAnalyzer.java
+++ src/main/java/StateCensusAnalyzer.java
@@ -1,17 +1,11 @@
-import java.io.FileWriter;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Comparator;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
-
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import CSVBuilderJar.CSVBuilderJar.*;
 
